feadf4dd7cc552325d2ae60d94589f0932b1d754
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2025-02-27 19:31:59 -0300

    feat: adding `hashing` in module `users`

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RedisService } from 'src/redis/redis.service';
+import { HashingService } from '../hashing/hashing.service';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, RedisService],
+  providers: [UsersService, PrismaService, RedisService, HashingService],
 })
 export class UsersModule { }
